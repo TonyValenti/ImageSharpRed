4d667e6ab29529823a5a355c2ab40ac36a856b15
Tony Valenti <Tony.Valenti@GMail.com>
2023-12-28 11:13:42 -0600

    Commit

---
+++ ConsoleApp2/ImageImageSource.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Bmp;
+using static System.Net.Mime.MediaTypeNames;
+
+public class ImageImageSource : MigraDocCore.DocumentObjectModel.MigraDoc.DocumentObjectModel.Shapes.ImageSource.IImageSource {
+    public SixLabors.ImageSharp.Image Image { get; }
+    public ImageImageSource (SixLabors.ImageSharp.Image Input){
+        this.Image = Input;
+        this.Width = Input.Width;
+        this.Height = Input.Height;
+        this.Transparent = false;
+        this.Name = $@"*{Guid.NewGuid()}";
+    }
+    
+    public int Width { get; }
+    public int Height { get; }
+    public string Name { get; }
+    public bool Transparent { get; }
+
+    public void SaveAsJpeg(MemoryStream ms) {
+        Image.SaveAsJpeg(ms);
+    }
+
+    public void SaveAsPdfBitmap(MemoryStream ms) {
+        BmpEncoder bmp = new BmpEncoder { BitsPerPixel = BmpBitsPerPixel.Pixel32 };
+        Image.Save(ms, bmp);
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PdfSharpCore.Drawing;
+var Doc = new PdfSharpCore.Pdf.PdfDocument();
+
+Console.WriteLine($@" .NET {System.Environment.Version}");
+
+var Files = new[] {
+    $@".\000809D7.jpg",
+};
+
+
+foreach (var File in Files) {
+
+    var Page = Doc.AddPage();
+    {
+
+        using var OriginalImage = SixLabors.ImageSharp.Image.Load(File);
+
+        var ImageSource = new ImageImageSource(OriginalImage);
+
+        using var Image = XImage.FromImageSource(ImageSource);
+
+        Page.Height = Image.PointHeight;
+        Page.Width = Image.PointWidth;
+
+        using var G = XGraphics.FromPdfPage(Page);
+        G.DrawImage(Image, 0, 0);
+    }
+}
+
+Doc.Save($@"C:\Test.pdf");
